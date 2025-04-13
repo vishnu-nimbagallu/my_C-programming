bf531a9f6dd2e03fa04fef77260ef654b498128a
NIMBAGALLUVISHNU <141856162+NIMBAGALLUVISHNU@users.noreply.github.com>
2025-04-13 23:55:35 +0530

    Add files via upload

---
+++ even_odd.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter the number:");
+	scanf("%d",&num);
+	if(num %2==0)
+	{
+		printf("the number is even");
+		
+	}
+	else
+	{
+		printf("the number is odd");
+	}
+	return 0;
+} 
