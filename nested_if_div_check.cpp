e55773561012d90b83164878de5a59183e3e3191
NIMBAGALLUVISHNU <141856162+NIMBAGALLUVISHNU@users.noreply.github.com>
2025-04-16 23:51:03 +0530

    Add files via upload

---
+++ nested_if_div_check.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+	int a=15;
+	printf("a: %d\n",a);
+	
+	if(a %2 == 0)
+	{
+		if(a%3 ==0)
+		{
+			printf("the number divisible by 2 and 3");
+		}
+		else
+		{
+			printf("the number divisible by 2 not by 3 ");
+		}
+	}
+	else{
+		if(a%3 == 0)
+		{
+			printf("the number divisible by 3 but not by 2");
+		}
+		else
+		{
+			printf("the number not divisible by 3 not divisible by 2");
+		}
+	}
+	return 0;
+	
+}
