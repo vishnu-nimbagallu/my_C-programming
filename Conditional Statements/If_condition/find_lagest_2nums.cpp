370ed15e32997abd1354132865ee5e16ebbb0e1a
vishnu-nimbagallu <141856162+vishnu-nimbagallu@users.noreply.github.com>
2025-07-02 23:31:02 +0530

    Add files via upload

---
+++ Conditional Statements/If_condition/find_lagest_2nums.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int num1;
+	int num2;
+	printf("enter the two numbers:");
+	scanf("%d %d",&num2,&num2);
+	if(num1>=num2 && num2>=num1)
+	{
+		printf("this num is bigger:%d",num1);
+	}
+	printf("this num is bigger:%d",num2);
+	return 0;
+	
+}
