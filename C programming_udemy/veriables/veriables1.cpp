a3e0ac19d078d5c029c6a415d7428d5b7b085ab5
vishnu-nimbagallu <141856162+vishnu-nimbagallu@users.noreply.github.com>
2025-07-15 23:40:06 +0530

    Add files via upload

---
+++ C programming_udemy/veriables/veriables1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int x=20;
+	printf("x= %d\n",x);
+	printf("address of x is = %u\n",&x);
+	
+	float y=19.99;
+	printf("y=%f\n",y);
+	printf("address of y is =%u\n",&y);
+	
+	char z='v';
+	printf("z=%c\n",z);
+	printf("address of z is =%u\n",&z);
+}
